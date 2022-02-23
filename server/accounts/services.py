e2d8ada0694cc7b04040a0470d1e637aaf6a739b
thebadfordota <sasha.skrynnik01@gmail.com>
2022-02-23 21:30:26 +0400

    fix settings for services

--- server/accounts/services.py
+++ server/accounts/services.py
@@ -1,9 +1,5 @@
-# from django.conf import settings
-# from server.settings import MONGODB_HOST, MONGODB_PORT, MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_DATABASE
+from django.conf import settings
 from pymongo import MongoClient
-# import os
-
-# settings.configure(default_settings="server.settings")
 
 
 class CountViewsServices:
@@ -12,16 +8,11 @@ class CountViewsServices:
     """
 
     def __init__(self):
-        # os.environ['DJANGO_SETTINGS_MODULE'] = 'server.settings'
-        # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
-        # print(settings.CELERY_BROKER_URL)
-        # Не получается импортировать из settings.py
-        # self.client = MongoClient(host=settings.MONGODB_HOST, port=settings.MONGODB_PORT,
-        #                           username=settings.MONGODB_USERNAME, password=settings.MONGODB_PASSWORD)
-        # self.collection = self.client[settings.MONGODB_DATABASE]['count_views']
-        self.client = MongoClient(host='mongo', port=27017,
-                                  username='admin', password='admin')
-        self.collection = self.client['insure_brother']['count_views']
+        self.client = MongoClient(host=settings.MONGODB_HOST,
+                                  port=settings.MONGODB_PORT,
+                                  username=settings.MONGODB_USERNAME,
+                                  password=settings.MONGODB_PASSWORD)
+        self.collection = self.client[settings.MONGODB_DATABASE]['count_views']
 
     def increase_count_views(self, product_id: int, product_name: str) -> None:
         if self.collection.count_documents({'_id': product_id}) == 0:
@@ -41,10 +32,10 @@ class CountViewsServices:
         return 0
 
     def delete_count_views_info(self, product_id: int) -> None:
-        pass
+        self.collection.delete_one({'_id': product_id})
 
-    def update_count_views_info(self, product_id: int) -> None:
-        pass
+    def update_count_views_info(self, product_id: int, product_name: str) -> None:
+        self.collection.update_one({'_id': product_id}, {'$set': {'name': product_name}})
 
 # client = MongoClient(host='mongo', port=27017,
 #                                   username='admin', password='admin')
